7411a451e61ae7f448d700ca751f4881454bfac9
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-04-04 21:09:23 -0400

    homework 3, using c++

---
+++ hw3/main.cpp
@@ -0,0 +1,57 @@
+#include <cstdio>
+#include <cstdlib>
+#include <pthread.h>
+
+int THREAD_COUNT{3};
+int ITERATIONS{5};
+int current_iteration{0};
+
+pthread_mutex_t mutex;
+pthread_cond_t cond;
+
+struct ThreadData {
+    int current;
+    int next;
+};
+
+void *routine(void *arg)
+{
+    auto *data = (ThreadData *) arg;
+    pthread_mutex_lock(&mutex);
+
+    for (int i = 0; i < ITERATIONS; i++){
+        while (current_iteration != data->current){
+            pthread_cond_wait(&cond, &mutex);
+        }
+        printf("Thread %d - iteration no. %d\n", data->current+1, i+1);
+        current_iteration = data->next;
+        pthread_cond_broadcast(&cond);
+    }
+    pthread_mutex_unlock(&mutex);
+    pthread_exit(nullptr);
+}
+
+int main () {
+    ThreadData data[THREAD_COUNT];
+    pthread_t threads[THREAD_COUNT];
+
+    pthread_mutex_init(&mutex, nullptr);
+    pthread_cond_init(&cond, nullptr);
+
+    for (int i = 0; i <= THREAD_COUNT - 1; i++) {
+        data[i].current = i;
+        data[i].next = (i == THREAD_COUNT - 1) ? 0 : i + 1;
+
+        if (pthread_create(&threads[i], nullptr, routine, (void *) &data[i]) != 0) {
+            printf("Issue with creating thread %d\n", i+1);
+            exit(-1);
+        }
+    }
+
+    for (int i = 0; i <= THREAD_COUNT - 1; i++)
+        if (pthread_join(threads[i], nullptr) != 0 ){
+            printf("Issue with joining thread %d\n", i+1);
+            exit(-1);
+        }
+    return 0;
+}
