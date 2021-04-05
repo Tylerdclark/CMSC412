0b2e7b4a72c90a3f810cdb399bce2fc1bf703bde
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-04-05 14:29:32 -0400

    slight formatting

--- hw3/main.cpp
+++ hw3/main.cpp
@@ -14,43 +14,42 @@ struct ThreadData {
     int next;
 };
 
-void *routine(void *arg)
-{
+void *routine(void *arg) {
     auto *data = (ThreadData *) arg;
     pthread_mutex_lock(&mutex);
 
-    for (int i = 0; i < ITERATIONS; i++){
-        while (current_iteration != data->current){
+    for (int i = 0; i < ITERATIONS; i++) {
+        while (current_iteration != data->current) {
             pthread_cond_wait(&cond, &mutex);
         }
-        printf("Thread %d - iteration no. %d\n", data->current+1, i+1);
+        printf("Thread %d - iteration no. %d\n", data->current + 1, i + 1);
         current_iteration = data->next;
-        pthread_cond_broadcast(&cond);
+        pthread_cond_broadcast(&cond); // unblock all threads
     }
     pthread_mutex_unlock(&mutex);
     pthread_exit(nullptr);
 }
 
-int main () {
+int main() {
     ThreadData data[THREAD_COUNT];
     pthread_t threads[THREAD_COUNT];
 
     pthread_mutex_init(&mutex, nullptr);
     pthread_cond_init(&cond, nullptr);
 
-    for (int i = 0; i <= THREAD_COUNT - 1; i++) {
+    for (int i = 0; i < THREAD_COUNT; i++) {
         data[i].current = i;
-        data[i].next = (i == THREAD_COUNT - 1) ? 0 : i + 1;
+        data[i].next = (i == THREAD_COUNT - 1) ? 0 : i + 1; // last one points to the first
 
         if (pthread_create(&threads[i], nullptr, routine, (void *) &data[i]) != 0) {
-            printf("Issue with creating thread %d\n", i+1);
+            printf("Issue with creating thread %d\n", i + 1);
             exit(-1);
         }
     }
 
     for (int i = 0; i <= THREAD_COUNT - 1; i++)
-        if (pthread_join(threads[i], nullptr) != 0 ){
-            printf("Issue with joining thread %d\n", i+1);
+        if (pthread_join(threads[i], nullptr) != 0) {
+            printf("Issue with joining thread %d\n", i + 1);
             exit(-1);
         }
     return 0;
