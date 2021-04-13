6ed26831b548565cc536c572f8ac4e2a6d92795b
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-04-12 23:34:05 -0400

    moved a ton of stuff around

--- hw4/src/main.cpp
+++ hw4/src/main.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
-#include <fstream>
+#include "bankers.h"
 
 using namespace std;
-namespace fs = std::filesystem;
 
-int menuPrompt() {
+void menuPrompt(bool &quit) {
     string ans{};
 
-    cout << "Would you like to keep going? Y/N\n";
+    cout << "Would you like to read in a file? Y/N\n";
     getline(cin >> ws, ans);
     if (ans != "y" || ans != "Y") {
-        return 1;
+        quit = false;
     } else {
-        return 0;
-    }
-}
-
-void read_file(const string &fileName) {
-    ifstream inFile1(fileName, ios::in);
-    if (!inFile1)
-        cerr << "File open error." << endl;
-    else {
-        char c;
-        while ((c = inFile1.get()) != EOF)
-            cout << c;
-        cout << endl;
-        inFile1.close();
+        quit = true;
     }
 }
 
 int main() {
-    std::cout << "Current path is " << fs::current_path() << '\n'; // (1)
-    while (menuPrompt()) {
-        string fileName;
+    int prcCt{0};
+    int resCt{0};
+    string fileName;
+    bool quit = false;
+
+    while (!quit) {
+        cout << "How many Processes?\n";
+        cin >> prcCt;
+        cout << "How many resources?\n";
+        cin >> resCt;
+        Bankers b{resCt, prcCt};
         cout << "What is the path to file?\n";
         cin >> fileName;
-        read_file(fileName);
+        b.readFile(fileName);
+        b.method();
+        b.display();
+        menuPrompt(quit);
     }
     return 0;
 }
