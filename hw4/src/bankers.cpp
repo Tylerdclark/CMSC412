c2bb902d3683c7ebe01936584db0de3a13b26920
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-04-12 23:33:14 -0400

    guess I need this

---
+++ hw4/src/bankers.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tyler Clark on 4/12/21.
+//
+
+#include "bankers.h"
---
+++ hw4/src/bankers.h
@@ -0,0 +1,14 @@
+//
+// Created by Tyler Clark on 4/12/21.
+//
+
+#ifndef HW4_BANKERS_H
+#define HW4_BANKERS_H
+
+
+class bankers {
+
+};
+
+
+#endif //HW4_BANKERS_H
