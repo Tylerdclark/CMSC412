e89da96535621a9866703639bd1e468dfaf55c08
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-05-07 17:49:09 -0700

    I should break these commits up

--- final-project/src/dev/tylerdclark/Menu.java
+++ final-project/src/dev/tylerdclark/Menu.java
@@ -1,4 +1,212 @@
+/* FILE: Menu.java
+ * AUTHOR: Tyler Clark
+ * DATE: 07 May 2021
+ */
+
 package dev.tylerdclark;
 
+import java.util.*;
+
+/**
+ * Menu class that will drive the program. Contains a loop and a {@link Scanner} to take input and interact with the
+ * user.
+ */
 public class Menu {
+
+    private final Scanner scanner;
+    private boolean keepGoing;
+    private int[] referenceString;
+    private int[][] matrix;
+    private int physicalFrames;
+    private final static int MIN_PHY_FRAME = 1;
+    private final static int MAX_PHY_FRAME = 8;
+    private final static int MIN_VIRT_FRAME = 0;
+    private final static int MAX_VIRT_FRAME = 9;
+
+
+    /**
+     * Constructor that will create an instance of the {@link Menu} class. Initializes a {@link Scanner} and the variable
+     * that the main menu loop uses. The user will input integers corresponding to menu choices with this object.
+     */
+    public Menu() {
+        scanner = new Scanner(System.in);
+        keepGoing = true;
+        referenceString = null;
+    }
+
+    /**
+     * commences the main loop and gets user input using {@link #userChoice()}
+     */
+    public void run() {
+        while(keepGoing){
+            System.out.println("*** Final Project - Demand Paging Simulator *** \nSelect from the following options:" +
+                    "\n0 - Exit \n1 - Read reference string \n2 - Generate reference string \n3 - Display current " +
+                    "reference string \n4 - Simulate FIFO \n5 - Simulate OPT \n6 - Simulate LRU \n7 - Simulate LFU"
+            );
+
+            keepGoing = userChoice();
+        }
+    }
+
+    /**
+     * Using a {@link Scanner}, this method collects the user input and calls the method according to the integer given.
+     * If the user gives something other than the valid options (0-7) a warning message will be displayed.
+     *
+     * @return whether or not to continue the program based on the user input
+     */
+    private boolean userChoice(){
+        try{
+            switch (scanner.nextInt()){
+                case 0:
+                    return false;
+                case 1:
+                    readRefString();
+                    break;
+                case 2:
+                    generateReferenceString();
+                    break;
+                case 3:
+                    showReferenceString();
+                    break;
+                case 4:
+                    simulateFIFO();
+                    break;
+                default:
+                    System.err.println("Please use 0-7");
+            }
+            
+        }catch (InputMismatchException exception){
+            System.err.println("Please use 0-7");
+            scanner.next(); // consume the current token
+            return true;
+        }
+        return true;
+    }
+
+
+    /**
+     * Prompts the user for a reference string and collects it using {@link Scanner}. This method then goes on to
+     * ensure the numbers passed are valid and within {@link #MIN_VIRT_FRAME} and {@link #MAX_VIRT_FRAME}. If everything
+     * is good, the reference string is stored in {@link #referenceString} as an int array.
+     *
+     * Also asks for physical frames and checks if they are within {@link #MIN_PHY_FRAME} and {@link #MAX_PHY_FRAME}.
+     */
+    private void readRefString() {
+
+        String userInput = null;
+        System.out.println("Please enter a reference string");
+        try {
+            scanner.nextLine();
+            userInput = scanner.nextLine();
+        }catch (NoSuchElementException exception){
+            System.out.println("You must enter something!");
+            exception.printStackTrace();
+        }
+        String[] refStringArr = userInput.split("\\s+");
+        referenceString = new int[refStringArr.length];
+        try{
+            for (int i = 0; i < refStringArr.length; i++) {
+                int tempInt  = Integer.parseInt(refStringArr[i]);
+                if (tempInt >= MIN_VIRT_FRAME && tempInt <= MAX_VIRT_FRAME){
+                    referenceString[i] = tempInt;
+                }else{
+                    referenceString[i] = -1; // -1 for any bad values
+                }
+
+            } //check if any are bad
+            if (Arrays.stream(referenceString).anyMatch( (e)-> e ==-1)){
+                referenceString = null;
+                System.err.println("Please use ten frames for the reference string numbered 0-9");
+                return;
+            }
+
+            // Do the physical frames
+            System.out.println("How many physical frames? (between 1 and 8)");
+            physicalFrames = scanner.nextInt();
+
+            if (physicalFrames <= MIN_PHY_FRAME || physicalFrames > MAX_PHY_FRAME){
+                System.err.println("Physical frames must be between 1 and 8");
+                physicalFrames = 0;
+                return;
+            }
+
+        }catch (NumberFormatException exception){
+            System.err.println("Trouble parsing Integers from reference");
+        }
+
+        System.out.println(Arrays.toString(referenceString));
+        System.out.println("Was added as the reference string.");
+    }
+
+    /**
+     * Creates a random reference string with physical frames
+     */
+    private void generateReferenceString() {
+        Random random = new Random();
+
+        System.out.println("What is the length of the new reference string?");
+        int newRefStringLength = scanner.nextInt();
+
+        if (newRefStringLength < 1){
+            System.out.println("Length needs to be greater than 1");
+        }else{
+            referenceString = new int[newRefStringLength];
+            for (int i = 0; i < newRefStringLength-1; i++) {
+                referenceString[i] = random.nextInt(10);
+            }
+
+            System.out.println("How many physical frames (between 1 and 8)");
+            //TODO: maybe wrap this in a try/catch
+            int response = scanner.nextInt();
+            if (response >= MIN_PHY_FRAME && response <= MAX_PHY_FRAME){
+                physicalFrames = response;
+                System.out.println("New reference string:");
+                System.out.println(Arrays.toString(referenceString));
+                System.out.println("Physical frame count:");
+                System.out.println(physicalFrames);
+            }else {
+                System.err.println("Physical frames must be between 1 and 8");
+                physicalFrames = 0;
+            }
+        }
+    }
+
+    /**
+     * Displays the stored reference string if it is stored or suggests the user to create one with options 1 or 2
+     * otherwise.
+     */
+    private void showReferenceString() {
+        if (referenceString != null){
+            System.out.println("The current reference string is :");
+            System.out.println(Arrays.toString(referenceString));
+        }else{
+            System.err.println("No current reference string! Create one with option 1 or 2");
+        }
+
+        if (physicalFrames == 0){
+            System.err.println("You should add a physical frame count using option 1 or 2 for this program to operate " +
+                    "correctly. The number should be between 1 and 8");
+        }else{
+            System.out.println("Physical frame count:");
+            System.out.println(physicalFrames);
+        }
+    }
+
+    private void simulateFIFO(){
+        if (setupSimulation()){
+            System.out.println(Arrays.deepToString(matrix));
+        }
+
+    }
+
+    private boolean setupSimulation(){
+        if (referenceString == null || physicalFrames == 0){
+            System.out.println("You need to have a valid reference string and set a physical frame between 1 and 8");
+            return false;
+        }else{ // +2 because of the victim row and page fault row
+            matrix = new int[physicalFrames+2][referenceString.length];
+            return true;
+        }
+
+    }
 }
