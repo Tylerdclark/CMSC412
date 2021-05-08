52ab7fd8dd261d8ebb591aa7af2977b3dfedd284
Tyler Clark <tylerc.thedeveloper@gmail.com>
2021-05-07 17:48:51 -0700

    add the run method

--- final-project/src/dev/tylerdclark/Main.java
+++ final-project/src/dev/tylerdclark/Main.java
@@ -1,8 +1,12 @@
+/* FILE: Main.java
+ * AUTHOR: Tyler Clark
+ * DATE: 07 May 2021
+ */
 package dev.tylerdclark;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Menu();
+        new Menu().run();
     }
 }
